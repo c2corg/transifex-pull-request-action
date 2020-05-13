9aca980ceb1f040620c7a8a889b26285561b409e
Bruno Besson <bruno.besson@viseo.com>
2020-05-13 09:37:59 +0200

    oops

--- dist/index.js
+++ dist/index.js
@@ -16526,12 +16526,12 @@ exports.default = utils_1.gql `
   query TransifexBranchQuery($owner: String!, $name: String!, $branch: String!) {
     repository(owner: $owner, name: $name) {
       id
-      refs(refPrefix: "refs/heads/", query: $branch, first: 1, states: [OPEN]) {
+      refs(refPrefix: "refs/heads/", query: $branch, first: 1) {
         totalCount
         edges {
           node {
             id
-            associatedPullRequests(first: 1) {
+            associatedPullRequests(first: 1, states: [OPEN]) {
               totalCount
               edges {
                 node {
